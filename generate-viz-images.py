a3d8a969cfcca05780e8a30a3625380ddc856328
Devin Lange <devinscottlange@gmail.com>
2020-08-20 11:11:56 -0600

    create main logic flow plus stubs

--- generate-viz-images.py
+++ generate-viz-images.py
@@ -1,11 +1,36 @@
+import os
+import sys
+import fnmatch
+# Matlab modules
 import h5py
 from scipy.io import loadmat
 
-baseFolder = './in/'
+
+def main(baseFolder: str) -> None:
+    pattern = 'data*.mat'
+    for root, _, files in os.walk(baseFolder):
+        for name in fnmatch.filter(files, pattern):
+            matlabFilename = os.path.join(root, name)
+            outFolderName = matlabFilename[:-4]
+            if not os.path.exists(outFolderName):
+                os.mkdir(outFolderName)
+
+            if shouldMakeFiles(matlabFilename, outFolderName):
+                makeFiles(matlabFilename, outFolderName)
+    return
 
 
-def main():
+def shouldMakeFiles(matlabFilename: str, outFolderName: str) -> bool:
+    # TODO
+    return False
+
+def makeFiles(matlabFilename: str, outFolderName: str) -> None:
+    # TODO
     return
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        print('Error: no input folder received. Quitting.')
+        sys.exit(1)
+
+    main(sys.argv[1])
