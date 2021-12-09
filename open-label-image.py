ef98756d4d271689e1edf3c78031b4b66c916482
Devin Lange <devinscottlange@gmail.com>
2021-12-09 11:45:22 -0700

    add basic example decoding image label data

---
+++ open-label-image.py
@@ -0,0 +1,32 @@
+
+import RLE_pb2
+
+inFolder = './in/data7/'
+outFolder = './out/'
+
+
+def main():
+    # Open file
+    testFile = inFolder + 'L0.pb'
+    f = open(testFile, 'rb')
+    pbDataString = f.read()
+    f.close()
+    
+    # parse protobuf structure
+    rleLabelData = RLE_pb2.ImageLabels()
+    rleLabelData.ParseFromString(pbDataString)
+
+    # Use RLE structure
+    # print(rleLabelData)
+    rowIndex = 0
+    for row in rleLabelData.rowList:
+        print('Row:', rowIndex)
+        rowIndex += 1
+        for labelRun in row.row:
+            print('\tL:', labelRun.label, '[{}, {}]'.format(labelRun.start, labelRun.start + labelRun.length))
+        if rowIndex >= 300:
+            break
+    return
+
+if __name__ == '__main__':
+    main()
